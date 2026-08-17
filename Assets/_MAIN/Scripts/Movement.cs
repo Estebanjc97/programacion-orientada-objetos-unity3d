5b409d01c9feb0ef7be12cbedd77122741c494c2
estebanImeropex <44655087+Estebanjc97@users.noreply.github.com>
2026-08-17 12:46:01 -0500

    Condicionales

--- Assets/_MAIN/Scripts/Movement.cs
+++ Assets/_MAIN/Scripts/Movement.cs
@@ -2,16 +2,62 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public float speed = 5f;
+    public float jump = 5f;
     public Rigidbody2D rigidbodyPlayer;
+
+    public float Xscale;
+
+    private float x;
+
+    //[Header("Ground Check")]
+    //public Transform groundCheck;      // objeto vac�o en los pies del jugador
+    //public float groundRadius = 0.2f;
+    //public LayerMask groundLayer;      // capa que representa el suelo
+
+    //private bool jumpRequest;
+    //private bool isGrounded;
 
     private void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        x = Input.GetAxisRaw("Horizontal");
 
-        rigidbodyPlayer.MovePosition(rigidbodyPlayer.position + new Vector2(x, y) * speed * Time.fixedDeltaTime);
+        rigidbodyPlayer.MovePosition(rigidbodyPlayer.position + new Vector2(x, 0f) * speed * Time.fixedDeltaTime);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Flip();
+        }
     }
 
+    private void Flip()
+    {
+        if (transform.localScale.x == Xscale)
+        {
+            transform.localScale = new Vector3(-Xscale, 1, 1);
+        } else
+        {
+            transform.localScale = new Vector3(Xscale, 1, 1);
+        }
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    //// Comprueba si el jugador est� tocando el suelo
+    //    //isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+
+    //    // Movimiento horizontal
+
+    //    //if (Input.GetKeyDown(KeyCode.Space)/* && isGrounded*/)
+    //    //{
+    //    //    Debug.Log("Jump");
+    //    //}
+    //    // Salto
+    //    if (jumpRequest)
+    //    {
+    //        Debug.Log("Actual Jump");
+    //        rigidbodyPlayer.linearVelocity = new Vector2(rigidbodyPlayer.linearVelocity.x, jump);
+    //        jumpRequest = false;
+    //    }
+    //}
 }
